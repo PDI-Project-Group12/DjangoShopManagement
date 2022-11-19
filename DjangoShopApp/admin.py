dbe5ca97657962493a0b2c4c3e23ce0ad8beb960
Shaik-Chapal <shaikchapal@hotmail.com>
2022-11-19 16:20:59 +0200

    ADmin Login

--- DjangoShopApp/admin.py
+++ DjangoShopApp/admin.py
@@ -3,3 +3,17 @@ from django.contrib import admin
 # Register your models here.
 
 
+from DjangoShopApp.models import Company, Product, ProductDetails, Employee, Customer, Bill, EmployeeSalary, BillDetails, CustomerRequest, CompanyAccount, CompanyBank, EmployeeBank
+
+admin.site.register(Company)
+admin.site.register(Product)
+admin.site.register(ProductDetails)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(Bill)
+admin.site.register(EmployeeSalary)
+admin.site.register(BillDetails)
+admin.site.register(CustomerRequest)
+admin.site.register(CompanyAccount)
+admin.site.register(CompanyBank)
+admin.site.register(EmployeeBank)
