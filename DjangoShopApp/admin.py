18d7bc79a9d93203b127cb36390e58152b4d8315
Shaik-Chapal <shaikchapal@hotmail.com>
2022-11-20 12:42:28 +0200

    small twist

--- DjangoShopApp/admin.py
+++ DjangoShopApp/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from DjangoShopApp.models import Company, Product, ProductDetails, Employee, Customer, Bill, EmployeeSalary, BillDetails, CustomerRequest, CompanyAccount, CompanyBank, EmployeeBank
+
 
 from DjangoShopApp.models import Company, Product, ProductDetails, Employee, Customer, Bill, EmployeeSalary, BillDetails, CustomerRequest, CompanyAccount, CompanyBank, EmployeeBank
 
-<<<<<<< HEAD
-=======
-from DjangoShopApp.models import Company, Product, ProductDetails, Employee, Customer, Bill, EmployeeSalary, BillDetails, CustomerRequest, CompanyAccount, CompanyBank, EmployeeBank
 
->>>>>>> 20b2cf0d6de0b58ab20bec664798ac394a843eaa
 admin.site.register(Company)
 admin.site.register(Product)
 admin.site.register(ProductDetails)
@@ -20,8 +18,5 @@ admin.site.register(BillDetails)
 admin.site.register(CustomerRequest)
 admin.site.register(CompanyAccount)
 admin.site.register(CompanyBank)
-<<<<<<< HEAD
 admin.site.register(EmployeeBank)
-=======
-admin.site.register(EmployeeBank)
->>>>>>> 20b2cf0d6de0b58ab20bec664798ac394a843eaa
+
